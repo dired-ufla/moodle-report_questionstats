b825cd53e665ddafbde69626b218f16599047863
Paulo <pauloafpjunior@hotmail.com>
2020-12-10 09:11:19 -0300

    first commit

---
+++ lang/en/report_questionstats.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Strings for component, language 'en'
+ *
+ * @package    report
+ * @copyright  2019 Paulo Jr
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'Question Usage Statistics';
+$string['lb_choose_ctype'] = 'Filter by content type';
+$string['lb_qtype_name'] = 'Question type';
+$string['lb_question_usage'] = 'Usage (%)';
+$string['lb_question_amount'] = 'Modules amount';
+$string['lb_all_ctype'] = 'None';
+$string['lb_format_ctype'] = 'Formatted text';
+$string['lb_table_ctype'] = 'Text with table';
+$string['lb_image_ctype'] = 'Text with image';
+$string['lb_course'] = 'Courses using the module: ';
+$string['lb_chart_serie'] = 'Question usage statistics';
+$string['btn_refresh'] = 'Refresh';
+$string['link_back'] = 'Back';
+$string['link_summary'] = 'View courses';
